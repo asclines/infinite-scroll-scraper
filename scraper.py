970771be25e554de9fe8f9d9617ecf35b8378f8f
asclines <injpk23@gmail.com>
2017-01-07 13:50:01 -0600

    Now gets a list of urls that apppear in the src attribute under img tags

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import time
 import argparse
+from urlparse import urlparse
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
@@ -18,16 +19,17 @@ def scrape(args):
     Entry method to all the real work.
     Epects args to be either filled from CLI argument parsing or from GUI.
     """
-    browser = webdriver.Chrome()
+    driver = webdriver.Chrome()
     try:
-        browser.get(args.url)
+        driver.get(args.url)
         time.sleep(1)
-        page = browser.find_element_by_tag_name("body")
+        page = driver.find_element_by_tag_name("body")
         scroll_page(page, args.pages)
+        scrape_media(driver)
     except WebDriverException as e:
         raise UserInputException("Invalid URL",e)
     finally:
-        browser.quit()
+        driver.quit()
 
 def scroll_page(page, count):
     """ Pages down count times on page."""
@@ -36,15 +38,30 @@ def scroll_page(page, count):
         time.sleep(0.2)
         count-=1
 
+def scrape_media(driver):
+    """ Goes through the element and extracts media links."""
+    # page_source = (driver.page_source).encode('utf-8')
+    find_img_elems(driver)
+    # body_html = driver.find_element_by_tag_name("body")
+
+def find_img_elems(driver):
+    """ Finds all elements with tag <img> and returns a list of the src."""
+    image_elems = driver.find_elements_by_tag_name('img')
+    results = []
+    for image_elem in image_elems:
+        image_url = image_elem.get_attribute("src")
+        results.append(image_url)
+    print(results)
 
 def get_args():
     """Return the arguments passed in through CLI, or the defaults."""
     parser = argparse.ArgumentParser(description='Scrapes media from website')
     parser.add_argument('-f','--folder', help='Path to the output folder where media should be saved to. ', type=str, default="./media/")
-    parser.add_argument('-p','--pages', help='Number of times to page down in browser.', type=int, default="10")
+    parser.add_argument('-p','--pages', help='Number of times to page down in browser.', type=int, default="0")
     parser.add_argument('-u','--url', help='URL to scrape.', type=str, required=True)
     return parser.parse_args()
 
 
 if __name__ == "__main__": # For future use when a GUI appears.
     scrape(get_args())
+    print("done")
