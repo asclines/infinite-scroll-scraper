e3b42d1ffceea5af19aeb4a15afef0078241dab7
mclines <michael.clines@ttu.edu>
2017-01-09 15:26:22 -0600

    Added check url button.

--- scraper/scraper_gui.py
+++ scraper/scraper_gui.py
@@ -1,30 +1,76 @@
 import tkinter as tk
+from tkinter import filedialog
+
+#Colors, fonts etc...
+COLOR = "#EAECEE"
+FONT = "Arial 14"
 
 #Create the main window
 window = tk.Tk()
-window.geometry("500x125")
-window.title("Infinite Image Scraper")
+window.geometry("550x125")
+window.title("Infinite Scroll Scraper")
+window.configure(background = COLOR)
 
-#URL label and entry
+
+
+#----------URL label and entry----------#
 urlLabel = tk.Label(window,
                     text = "URL to be scraped:",
-                    font = "Verdana 14").grid(row = 0, column = 0)
+                    font = FONT,
+                    bg = COLOR).grid(row = 0, column = 0)
 
-urlEntry = tk.Entry(window).grid(row = 0, column = 1)
+urlEntry = tk.Entry(window,
+                    highlightbackground = COLOR,
+                    font = FONT).grid(row = 0, column = 1)
 
-#pages label and entry
+urlButton = tk.Button(window,
+                      text = "Check URL",
+                      highlightbackground = COLOR).grid(row = 0, column = 2)
+
+
+#----------pages label and entry----------#
 pagesLabel = tk.Label(window,
-                      text = "Number of pages to be scraped: ",
-                      font = "Verdana 14").grid(row = 1, column = 0)
-pagesEntry = tk.Entry(window).grid(row = 1, column = 1)
+                      text = "Number of times to page down: ",
+                      font = FONT,
+                      bg = COLOR).grid(row = 1, column = 0)
 
-#saveLocation label and entry
+pagesEntry = tk.Entry(window,
+                      highlightbackground = COLOR,
+                      font = FONT)
+pagesEntry.grid(row = 1, column = 1)
+
+
+#-----------saveLocation label and entry and button to access directory-------
+
 saveLocationLabel = tk.Label(window,
                              text = "Save location:",
-                             font = "Verdana 14").grid(row = 2, column = 0)
-saveLocationEntry = tk.Entry(window).grid(row = 2, column = 1)
+                             font = FONT,
+                             bg = COLOR).grid(row = 2, column = 0)
 
-#Execute button
-scrapeButton = tk.Button(window,text = "SCRAPE").grid(row = 3, column = 1)
+#Seperate initialization of object and inserting it to maintain variable type
+saveLocationEntry = tk.Entry(window,
+                             highlightbackground = COLOR,
+                             font = FONT)
+saveLocationEntry.grid(row = 2, column = 1)
+
+#function for saveLocationButton; pulls up folder directory
+def findDirectory():
+    file_path = filedialog.askdirectory()
+    saveLocationEntry.delete(0, tk.END)
+    saveLocationEntry.insert(0, str(file_path))
+    return None
+
+saveLocationButton = tk.Button(window,
+                               text = 'Folder',
+                               highlightbackground = COLOR,
+                               command = lambda: findDirectory()).grid(row = 2, column = 2)
+
+#----------Final button to execute program----------#
+scrapeButton = tk.Button(window,
+                         text = "Scrape",
+                         highlightbackground = COLOR,
+                         font = FONT).grid(row = 3, column = 1)
+
+
 #Run the program
 window.mainloop()
