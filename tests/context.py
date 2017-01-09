bba428cbb4e36a843afa887a547b0d27e200c5b4
asclines <injpk23@gmail.com>
2017-01-09 17:38:24 -0600

    OMG, maybe this time EVERYBODY WILL BE HAPPY WITH THE IMPORTS

--- tests/context.py
+++ tests/context.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('./scraper'))
 from scraper import scraper
 from scraper.utils import files
 from scraper.utils import urls
